366270209106d18196260133a5716a29cd8da3c2
imufun <imufun1@gmail.com>
2015-04-20 21:50:44 +0600

    // The ListAdapter acts as a bridge between the data and each ListItem         // You fill the ListView with a ListAdapter. You pass it a context represented by         // this. A Context provides access to resources you need.         // android.R.layout.simple_list_item_1 is one of the resources needed.         // It is a predefined layout provided by Android that stands in as a default

--- app/src/main/java/com/imufun/imran/listview_imgview_layinf_view_viewgrp_more/MainActivity.java
+++ app/src/main/java/com/imufun/imran/listview_imgview_layinf_view_viewgrp_more/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +15,24 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        // Simple array with a list of my favorite TV shows
+        String[] MyTvShow = {
+                "Pushing Daisies", "Better Off Ted", "Twin Peaks", "Freaks and Geeks", "Orphan Black", "Walking Dead",
+                "Breaking Bad", "The 400", "Alphas", "Life on Mars"
+        };
+
+
+        // The ListAdapter acts as a bridge between the data and each ListItem
+        // You fill the ListView with a ListAdapter. You pass it a context represented by
+        // this. A Context provides access to resources you need.
+        // android.R.layout.simple_list_item_1 is one of the resources needed.
+        // It is a predefined layout provided by Android that stands in as a default
+
+
+        ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, MyTvShow);
+        ListView listView = (ListView) findViewById(R.id.thelistView);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
